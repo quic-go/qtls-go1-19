db6227bf62d9e0ecf845e0f1d396bc2f1b1232f3
Marten Seemann <martenseemann@gmail.com>
2020-08-16 22:27:17 +0700

    use a tls.Config and a tls.ClientHelloInfo

--- handshake_server_test.go
+++ handshake_server_test.go
@@ -1505,14 +1505,14 @@ var getConfigForClientTests = []struct {
 			for i := range config.SessionTicketKey {
 				config.SessionTicketKey[i] = byte(i)
 			}
-			config.sessionTicketKeys = nil
+			fromConfig(config).sessionTicketKeys = nil
 		},
 		func(clientHello *ClientHelloInfo) (*Config, error) {
 			config := testConfig.Clone()
 			for i := range config.SessionTicketKey {
 				config.SessionTicketKey[i] = 0
 			}
-			config.sessionTicketKeys = nil
+			fromConfig(config).sessionTicketKeys = nil
 			return config, nil
 		},
 		"",
@@ -1534,7 +1534,7 @@ var getConfigForClientTests = []struct {
 		},
 		func(clientHello *ClientHelloInfo) (*Config, error) {
 			config := testConfig.Clone()
-			config.sessionTicketKeys = nil
+			fromConfig(config).sessionTicketKeys = nil
 			return config, nil
 		},
 		"",
@@ -1812,7 +1812,7 @@ func TestAESCipherReordering(t *testing.T) {
 			hasAESGCMHardwareSupport = tc.serverHasAESGCM
 			hs := &serverHandshakeState{
 				c: &Conn{
-					config: &Config{
+					config: &config{
 						CipherSuites: tc.serverCiphers,
 					},
 					vers: VersionTLS12,
@@ -1911,7 +1911,7 @@ func TestAESCipherReorderingTLS13(t *testing.T) {
 			hasAESGCMHardwareSupport = tc.serverHasAESGCM
 			hs := &serverHandshakeStateTLS13{
 				c: &Conn{
-					config: &Config{},
+					config: &config{},
 					vers:   VersionTLS13,
 				},
 				clientHello: &clientHelloMsg{
